40804eb88cbd34b74b3a96d1b97269a9e6e25d92
Erica Nshimirimana <e.nshimirim@alustudent.com>
2023-11-26 00:12:45 +0200

    codes

--- eyt/myapp/views.py
+++ eyt/myapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 # Create your views here.
 def index(request):
-  return  render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details ="our service is quick"
+    return  render(request, 'index.html', {'feature': feature1})
+
+def counter(request):
+    text = request.POST['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
